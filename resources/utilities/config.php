af7dc81407204180da0dd0f1b39a6b218a7d7c29
Angel Torres <at.lopez@outlook.com>
2020-03-03 13:04:59 -0600

    delete config.php

--- resources/utilities/config.php
+++
@@ -1,6 +0,0 @@
-<?php  
-    define("NOMBRE_HOST", "127.0.0.1");// Nombre del host
-    define("BASE_DE_DATOS", "investigaciones"); // Nombre de la base de modelos
-    define("USUARIO", "root"); // Nombre del usuario
-    define("CONTRASENA", "toortoor"); // Constraseña
-?>
